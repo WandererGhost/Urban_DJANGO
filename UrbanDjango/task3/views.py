40a896c0bed41bfe95f714ce59e8b6cbf74d7099
WandererGhost <pv_tanuha@mail.ru>
2025-01-23 14:17:21 +0500

    Create views.py

---
+++ UrbanDjango/task3/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+def order(request):
+    p1 = 'Печать документов'
+    p2 = 'Печать фотографий'
+    p3 = 'Печать на кружках'
+    p4 = 'Изготовление визиток'
+    p5 = 'Широкоформатная печать'
+    context = {
+        'p1': p1,
+        'p2': p2,
+        'p3': p3,
+        'p4': p4,
+        'p5': p5
+    }
+    return render(request, 'place_an_order.html', context)
+
+
+def get_contact(request):
+    return render(request, 'contacts.html')
